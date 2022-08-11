128bf6d545607ea9a720fcb29cf242f7cd172033
Bilal Güler <guleerbilal@gmail.com>
2022-08-11 14:37:19 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MERHABA PROGRAMIMA HOŞ GELDİNİZ
+
+* [**Patika Profil Linkim**](https://app.patika.dev/guleerbilal)
+
+* [**LinkedIn Profil Linkim**](https://www.linkedin.com/in/bilal-guler/)
+
+* Bu programım 1-100 arasında olan asal sayıları buluyor ve onların çıktısını alıyor.7
+
+## *Kodlarım*
+
+*![Kodlarım](img/kodlar.PNG)
+
+*![Output](img/output.PNG)
+
+
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            boolean asal = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    asal =  false;
+                }
+            }
+            if (asal) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+}
